2f367290f9384d140bb88b872fc7786e9ac64c53
fatih <rinqtf@gmail.com>
2025-10-13 21:59:06 +0300

    feat(middleware): log non-200 responses

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-import express from "express";
+import express, { NextFunction, Response, Request } from "express";
+
+type Middleware = (req: Request, res: Response, next: NextFunction) => void;
+
+const middlewareLogResponses: Middleware = (req, res, next) => {
+  res.on("finish", () => {
+    const status = res.statusCode;
+    if (status !== 200) {
+      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${status}`);
+    }
+  });
+  next();
+};
 
 const app = express();
 const PORT = 8080;
+
+app.use(middlewareLogResponses);
 
 app.use("/app", express.static("./src/app"));
 
