fb614007a69dbac72465fdcba391c82e2c5b340f
fatih <rinqtf@gmail.com>
2025-10-13 21:30:29 +0300

    refactor(structure): move static files to src/app

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", (_, res) => {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send("OK");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
